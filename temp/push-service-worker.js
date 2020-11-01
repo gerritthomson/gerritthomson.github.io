8d5d8d6429cf837c93eb892718734c11ec837db5
gerrit thomson <gerrit.thomson@gmail.com>
2020-11-01 13:31:35 +1100

    Store subscription.

--- temp/push-service-worker.js
+++ temp/push-service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('push', function (event) {
     const payload = event.data ? event.data.text() : 'no payload';
     let reading = JSON.parse(payload);
-    let notice = 'Dev:'+ reading.deviceId + ' Temp:' + temperature;
+    let notice = 'Dev:'+ reading.deviceId + ' Temp:' + reading.temperature;
     if( reading.insideTemp != undefined){
         notice += ' Inside:' + reading.insideTemp;
     }

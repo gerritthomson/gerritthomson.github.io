0718d19075e1e964bf08dea69064fa175e7cf075
gerrit thomson <gerrit.thomson@gmail.com>
2020-11-01 13:18:34 +1100

    Store subscription.

--- temp/push-service-worker.js
+++ temp/push-service-worker.js
@@ -1,8 +1,13 @@
 self.addEventListener('push', function (event) {
     const payload = event.data ? event.data.text() : 'no payload';
+    let reading = JSON.parse(payload);
+    let notice = 'Dev:'+ reading.deviceId + ' Temp:' + temperature;
+    if( reading.insideTemp != undefined){
+        notice += ' Inside:' + reading.insideTemp;
+    }
     event.waitUntil(
         self.registration.showNotification('ServiceWorker Cookbook', {
-            body: payload,
+            body: notice,
         })
     );
 });
